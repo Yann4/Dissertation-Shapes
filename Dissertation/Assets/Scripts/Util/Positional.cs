cbaa206be4cbb016a67550bea17f3da1e1a6ebbb
Yann Richeux <richeux4@gmail.com>
2019-10-05 15:29:39 +0100

    Missed from utils commit

---
+++ Dissertation/Assets/Scripts/Util/Positional.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Dissertation.Util
+{
+	public static class Positional
+	{
+		public enum Direction
+		{
+			Left,
+			Right
+		}
+
+		public static bool IsAtPosition(Transform transform, Vector3 position, float tolerance = 1.0f)
+		{
+			return Vector3.SqrMagnitude(transform.position - position) < (tolerance * tolerance);
+		}
+	}
+}
