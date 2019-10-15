f4e1c2e668a09cd53fa46c6a62d16444da81703c
Yann4 <richeux4@gmail.com>
2019-10-15 20:11:07 +0100

    Add LineDrawer to connect gameobjects in UI space & use it for AgentDebug to show line of sight

--- Dissertation/Assets/Scripts/UI/AgentDebugUI.cs
+++ Dissertation/Assets/Scripts/UI/AgentDebugUI.cs
@@ -1,4 +1,7 @@
-﻿using Dissertation.Character.AI;
+﻿using Dissertation.Character;
+using Dissertation.Character.AI;
+using Dissertation.Util;
+using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,6 +19,7 @@ namespace Dissertation.UI
 		[SerializeField] private Text _health;
 
 		private AgentController _owner;
+		private LineDrawer _drawer;
 
 		private const string _hostileText = "<color=red>Hostile to player</color>";
 		private const string _friendlyText = "<color=green>Friendly to player</color>";
@@ -32,6 +36,8 @@ namespace Dissertation.UI
 			TrackObject(_owner.transform);
 
 			RefreshText();
+
+			_drawer = Camera.main.GetComponent<LineDrawer>();
 		}
 
 		protected override void Update()
@@ -39,6 +45,16 @@ namespace Dissertation.UI
 			base.Update();
 
 			RefreshText();
+
+			List<BaseCharacterController> visibleCharacters = App.AIBlackboard.GetVisibleCharacters(_owner);
+			GameObject[] visible = new GameObject[visibleCharacters.Count];
+			for(int idx = 0; idx < visibleCharacters.Count; idx++)
+			{
+				visible[idx] = visibleCharacters[idx].gameObject;
+			}
+
+			LineDrawer.Graph graph = new LineDrawer.Graph() { CentrePoint = _owner.gameObject, Points = visible };
+			_drawer.AddGraph(graph);
 		}
 
 		private void RefreshText()
@@ -50,6 +66,16 @@ namespace Dissertation.UI
 			_hostility.text = App.AIBlackboard.IsHostileToPlayer(_owner) ? _hostileText : _friendlyText;
 
 			_health.text = _owner.Health.IsDead ? _deadText : string.Format(_healthText, _owner.Health.CurrentHealth, _owner.Config.MaxHealth);
+		}
+
+		protected override void SetVisible(bool visible)
+		{
+			base.SetVisible(visible);
+
+			if(!visible)
+			{
+				_drawer.RemoveGraph(_owner.gameObject);
+			}
 		}
 
 		private string ConstructString(string title, State[] states)
