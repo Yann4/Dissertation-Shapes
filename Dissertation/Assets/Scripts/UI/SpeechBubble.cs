f831a315716a750fe3234e6bb320f34596c883d1
Yann4 <richeux4@gmail.com>
2020-01-31 08:45:21 +0000

    Add automatic text wrapping to the speech bubbles to improve readability

--- Dissertation/Assets/Scripts/UI/SpeechBubble.cs
+++ Dissertation/Assets/Scripts/UI/SpeechBubble.cs
@@ -1,6 +1,7 @@
 ﻿using Dissertation.Util;
 using System;
 using System.Collections;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,6 +20,10 @@ namespace Dissertation.UI
 		private float _showDuration = 5.0f;
 		private const float _wordsPerSecond = 60.0f / 100.0f; //Google says average adult reads 200-250 wpm, but that seems a bit quick so slow it down
 
+		//Max default line width
+		private const int _characterWidth = 30;
+		private Vector3 _heightOffset = Vector3.zero;
+
 		private Func<bool> _shouldCloseEarly;
 
 		private Coroutine _present = null;
@@ -34,7 +39,23 @@ namespace Dissertation.UI
 
 		public void Show( Transform actor, string text, float showDuration, Func<bool> shouldCloseEarly = null )
 		{
-			_text.text = text;
+			_heightOffset = Vector3.zero;
+
+			string toShow = string.Empty;
+			if(text.Contains("/n"))
+			{
+				//If the string has formatting in it, respect that
+				toShow = text;
+			}
+			else
+			{
+				toShow = WordWrap(text, _characterWidth);
+			}
+
+			int newLineCount = text.Length - text.Replace("/n", "").Length;
+			_heightOffset.y = newLineCount * 31f; //31 is a magic number that's currently the height of a single line
+
+			_text.text = toShow;
 			TrackObject(actor);
 
 			_showDuration = showDuration;
@@ -86,6 +107,10 @@ namespace Dissertation.UI
 				StopCoroutine(_present);
 				StartCoroutine(FadeOut());
 			}
+
+			//This is an extra offset to make sure that the centre of the bubble
+			//is in roughly the same place no matter how many lines the text has
+			_rectTransform.localPosition += _heightOffset;
 		}
 
 		public override void CloseMenu()
@@ -109,16 +134,128 @@ namespace Dissertation.UI
 			{
 				// check if current char is part of a word
 				while (index < text.Length && !char.IsWhiteSpace(text[index]))
+				{
 					index++;
+				}
 
 				wordCount++;
 
 				// skip whitespace until next word
 				while (index < text.Length && char.IsWhiteSpace(text[index]))
+				{
 					index++;
+				}
 			}
 
 			return wordCount;
 		}
+
+		/*
+		 * Word wrap code shamelessly liberated from
+		 * https://www.codeproject.com/Articles/51488/Implementing-Word-Wrap-in-C
+		 * because word wrap is hard. This will only work for latin languages,
+		 * and probably not all of them
+		*/
+
+		/// <summary>
+		/// Word wraps the given text to fit within the specified width.
+		/// </summary>
+		/// <param name="text">Text to be word wrapped</param>
+		/// <param name="width">Width, in characters, to which the text
+		/// should be word wrapped</param>
+		/// <returns>The modified text</returns>
+		public static string WordWrap(string text, int width)
+		{
+			// Lucidity check
+			if (width < 1 || text.Length < width)
+			{
+				return text;
+			}
+
+			int next;
+			StringBuilder sb = new StringBuilder();
+
+			// Parse each line of text
+			for (int pos = 0; pos < text.Length; pos = next)
+			{
+				// Find end of line
+				int eol = text.IndexOf(System.Environment.NewLine, pos);
+				if (eol == -1)
+				{
+					eol = text.Length;
+					next = eol;
+				}
+				else
+				{
+					next = eol + System.Environment.NewLine.Length;
+				}
+
+				// Copy this line of text, breaking into smaller lines as needed
+				if (eol > pos)
+				{
+					do
+					{
+						int len = eol - pos;
+						if (len > width)
+						{
+							len = BreakLine(text, pos, width);
+						}
+
+						sb.Append(text, pos, len);
+						if (pos + len < text.Length) //Don't add a trailing newline
+						{
+							sb.Append(System.Environment.NewLine);
+						}
+
+						// Trim whitespace following break
+						pos += len;
+						while (pos < eol && Char.IsWhiteSpace(text[pos]))
+						{
+							pos++;
+						}
+					} while (eol > pos);
+				}
+				else
+				{
+					sb.Append(System.Environment.NewLine); // Empty line
+				}
+			}
+
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Locates position to break the given line so as to avoid
+		/// breaking words.
+		/// </summary>
+		/// <param name="text">String that contains line of text</param>
+		/// <param name="pos">Index where line of text starts</param>
+		/// <param name="max">Maximum line length</param>
+		/// <returns>The modified line length</returns>
+		private static int BreakLine(string text, int pos, int max)
+		{
+			// Find last whitespace in line
+			int i = max;
+			while (i >= 0 && !Char.IsWhiteSpace(text[pos + i]))
+			{
+				i--;
+			}
+
+			// If no whitespace found, break at maximum length
+			if (i < 0)
+			{
+				return max;
+			}
+
+			// Find start of whitespace
+			while (i >= 0 && Char.IsWhiteSpace(text[pos + i]))
+			{
+				i--;
+			}
+
+			// Return length of text before whitespace
+			return i + 1;
+		}
 	}
 }
+ 
